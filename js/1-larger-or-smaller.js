8be37626da5c42e3500afc8af8a4d49a8f315cbf
Eias <aboomar5464@gmail.com>
2025-09-29 16:27:49 -0700

    add larger or smaller code

---
+++ js/1-larger-or-smaller.js
@@ -0,0 +1,10 @@
+let num1 = parseInt(prompt("Enter the first integer:"));
+let num2 = parseInt(prompt("Enter the second integer:"));
+
+if (num1 > num2) {
+  document.write("The larger number is: " + num1);
+} else if (num2 > num1) {
+  document.write("The larger number is: " + num2);
+} else {
+  document.write("Both numbers are equal: " + num1);
+}
